a1ab5ebb243be7436cc7ecb68c3c20b3003b9b4a
saurav_medisys <saurav@medisys.com.sa>
2016-01-06 12:24:48 +0600

    06DEC2015 12:24 PM

---
+++ app/view/main/MainModel.js
@@ -0,0 +1,14 @@
+/**
+ * This class is the view model for the Main view of the application.
+ */
+Ext.define('extjsui.view.main.MainModel', {
+    extend: 'Ext.app.ViewModel',
+
+    alias: 'viewmodel.main',
+
+    data: {
+        name: 'extjsui'
+    }
+
+    //TODO - add data, formulas and/or methods to support your view
+});
